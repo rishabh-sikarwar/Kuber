584afbff57e8258e0a23b2dc8d716dddc26826dc
Rishabh Sikarwar <rishabhsikarwar1000@gmail.com>
2025-04-27 15:21:34 +0530

    update dashboard

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { BarLoader } from 'react-spinners';
+import DashboardPage from './page';
 
 const layout = ({children}) => {
   return (
-    <div>{children}</div>
-  )
+    <div className="px-5">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold tracking-tight gradient-title">
+          Dashboard
+        </h1>
+      </div>
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
 }
 
 export default layout
